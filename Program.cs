134636d5d9a6a7750857733d8225601faa9b7e2e
Aygun <sahmuradova18@gmail.com>
2024-05-07 10:04:30 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace LabTask29
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("ZooShop");
+			
+				Console.WriteLine("Zehmet olmasa secim edin: ");
+			    check:
+				Console.WriteLine("1. Register");
+				Console.WriteLine("2. Login");
+				Console.WriteLine("3. Exit");
+				List<User> users = new List<User>();
+			    User user = new User();
+
+			    string choice = Console.ReadLine();
+
+				switch (choice)
+				{
+					case "1":
+						Console.WriteLine("Adinizi daxil edin: ");
+						string name = Console.ReadLine();
+						Console.WriteLine("Soyadinizi daxil edin: ");
+						string surname = Console.ReadLine();
+						Console.WriteLine("Emailinizi daxil edin:");
+						string email = Console.ReadLine();
+						Console.WriteLine("Shifrenizi daxil edin: ");
+						string password = Console.ReadLine();
+					   user.Register(name, surname, email, password, Role.Admin);
+					goto check;
+				case "2":
+						Console.WriteLine("Emailinizi daxil edin: ");
+						string email1 = Console.ReadLine();
+						Console.WriteLine("Sifreni daxil edin: ");
+						string password1 = Console.ReadLine();
+						user.Login(email1, password1);
+						break;
+					case "3":
+						Environment.Exit(0);
+						//Console.WriteLine("Cixis etdiniz..");
+						break;
+					default:
+						Console.WriteLine("Yanlis secim");
+						break;
+				}
+			
+		}
+	}
+}
---
+++ User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace LabTask29
+{
+	internal class User
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public Role Role { get; set; }
+		public static List<User> users { get; set; } = new();
+        
+        public void Register(string name, string surname, string email, string password, Role role)
+		{
+			bool checkEmail = IsValidEmail(email);
+			bool checkPassword = IsValidPassword(password);
+			if (!checkEmail)
+			{
+				Console.WriteLine("Emailin formati duzgun deyil..");
+			}
+			if (!checkPassword)
+			{
+				Console.WriteLine("Sifrenin formati duzgun deyil..");
+			}
+			if (!checkPassword || !checkEmail)
+			{
+				return;
+			}
+			users.Add(new User {Name= name,Surname= surname, Email=email, Password=password });
+            Console.WriteLine("Qeydiyyatdan kecdiniz..");
+        }
+		public bool IsValidPassword(string password)
+		{
+			bool checkIsUpper = false;
+			bool checkIsLower = false;
+
+			if (password.Length >= 8)
+			{	
+				foreach (var p in password)
+				{
+					if (char.IsUpper(p))
+					{
+						checkIsUpper = true;
+					}
+					if (char.IsLower(p))
+					{
+						checkIsLower = true;
+					}
+					
+				}
+			}
+			return checkIsLower && checkIsUpper;
+		}
+		public bool IsValidEmail(string email)
+		{
+			return email.Contains("gmail.com");
+		}
+		public void Login(string email,string password)
+		{
+			if (users == null)
+			{
+                Console.WriteLine("Bele user yoxdur");
+				return;
+            }
+			foreach (var user in users)
+			{
+				if (user.Email.Equals(email) && user.Password.Equals(password))
+				{
+					Console.WriteLine("Giris ugurlu oldu..");
+				}
+				else
+				{
+					Console.WriteLine("Email ve ya sifre duzgun deyil. Yeniden cehd edin");
+				}
+			}
+			
+		}
+	}
+}
